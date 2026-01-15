aa4470da2bedb758296d3f6d11466fb3cd866fd2
GustavoSilva11620 <shark11620@sharkcoders.pt>
2026-01-15 22:17:31 +0000

    Update mechanics.py

--- mechanics.py
+++ mechanics.py
@@ -20,6 +20,8 @@ def usarpocao(vida_atual):
     vida_atual+=15
     if vida_atual >50:
         vida_atual=50
+        return vida_atual
 def  menu_combate():
     print(f"Escolha a sua opcao:\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n1-Ataque\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n2-Cura\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n3-Fugir")
+
 
