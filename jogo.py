e63abd34f974bb6d1f1ee6619fcf823d2a4baf91
GustavoSilva11620 <shark11620@sharkcoders.pt>
2026-01-15 22:13:02 +0000

    Add files via upload

--- jogo.py
+++ jogo.py
@@ -5,8 +5,38 @@ import time
 vidaMago=50
 ManaMago=30
 MonstroVida=60
-while vidaMago >=0:
-    mechanics.menu_combate()
+mechanics.menu_combate()
+while vidaMago >=0 or MonstroVida >=0:
     opcao=int(input())
     if opcao==1:
-        mechanics.calcular_dano(randint(1,15))
+        if ManaMago>5:
+            print("A conjurar",end="",flush=True)
+            dano=mechanics.calcular_dano(randint(10,20,),5)
+            for x in range(1,3):
+                sleep(0.5)
+                print(".",end="",flush=True)
+            MonstroVida-=dano
+            print(f"Bom trabalho!Deste {dano} de dano ao monstro! Ele esta agr com {MonstroVida} de hp!Tas com{ManaMago} de mana")
+        else:
+            print(f"Mana insuficiente! Tens {ManaMago} de Mana!!")
+    elif opcao==2:
+        if ManaMago>10:
+            mechanics.usarpocao(vidaMago)
+            print(f"Mago usa pocao! Mago recupera 15 de hp! Vida: {vidaMago}")
+        else:
+            print(f"Mana insuficiente! Tens {ManaMago} de Mana!!")
+    elif opcao==3:
+        print("Fugiste wow....Que divertido que foi ne :/ VOLTA A JOG")
+        break
+    else :
+        print("Opcao invalida!")
+    if MonstroVida>0:
+        danM=mechanics.calcular_dano(randint(10,25),5)
+        vidaMago-=danM
+        print(f"Montro atacou!!Dano:{danM} Vida: {vidaMago}")
+    if MonstroVida<0:
+        print("MAGO GANHOU A BATALHAAAAAAAAAAAAAAAAA yay")
+    if vidaMago<0:
+        print("Mago perdeu ToT")
+
+
